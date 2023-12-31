4d1983738131432df8d074adc810149fa0383b6c
livia <livia.gu@notitia.consulting>
2024-01-01 00:39:30 +1030

    change

--- rowSumOddNumbers/rowSumOddNumbers.go
+++ rowSumOddNumbers/rowSumOddNumbers.go
@@ -1,13 +1,5 @@
 package rowsumoddnumbers
 
 func RowSumOddNumbers(n int) int {
-	// get the first number of the row
-	// first number 1+(n-1)*n 1=> 1, 2=> 3, 3=> 7, 4=> 12, 5=> 21
-	firstNumber := 1 + (n-1)*n	
-	sum := firstNumber
-	for i := 1; i <= n; i++ {
-		numberAfter := firstNumber + 2*i
-		sum += numberAfter
-	}
 	return n*n*n
 }
